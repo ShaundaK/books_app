762bf4bde4c7781cfa5f81d1aa28453222906bee
ShaundaK <sdking1913@gmail.com>
2018-10-29 18:42:19 -0500

    show index

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -3,4 +3,10 @@ class Api::BooksController < ApplicationController
     @books = Book.all
     render "index.json.jbuilder"
   end
+
+  def show
+    book_id = params[:id]
+    @book = Book.find_by(id: book_id)
+    render "show.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
   get "/books" => "books#index"
+  get "/books/:id" => "books#show"
  end
 end
